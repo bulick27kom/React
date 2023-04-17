123dbed3bd2c2ef06804480c5ecbcff038474b51
Roman Brauer <bulick27kom@gmail.com>
2023-04-17 21:22:28 +0300

    second commit

--- 05-react-jsx-green-button/script.js
+++ 05-react-jsx-green-button/script.js
@@ -1,10 +1,21 @@
-const App = (
-    <div className="app">
-        <button onClick="">
-            Click me
-        </button>
-    </div>
-)
+
+
+const App = () => {
+    let buttonText = 'Click me';
+    console.log('Called');
+    const onButtonClick = () => {
+        buttonText = 'hello from React'
+        console.log('buttonText :>> ', buttonText);
+    }
+    return (
+        <div className="app">
+            <button onClick={onButtonClick}>
+                {buttonText}
+            </button>
+        </div>
+    )
+}
+
 const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container);
-root.render(App)
+root.render(<App />)
