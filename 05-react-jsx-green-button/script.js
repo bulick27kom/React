7768599d29ce5db074d0bf20c93707713965f78a
Roman Brauer <bulick27kom@gmail.com>
2023-04-19 20:07:09 +0300

    Finished

--- 05-react-jsx-green-button/script.js
+++ 05-react-jsx-green-button/script.js
@@ -1,16 +1,23 @@
 
 //let counter = 0;
-const App = () => {
-    const [buttonText, setButtonText] = React.useState('Click me')
-    
+const App = (props) => {
+    console.log('props :>> ', props);
+    const { initialbuttonText } = props
+    const [buttonText, setButtonText] = React.useState(initialbuttonText)
+    const [classesList, setClassesList] = React.useState('');
+    const [counter, setCounter] = React.useState(props.counter);
+
     const onButtonClick = () => {
-        setButtonText('Hello from React')
-       // setButtonText((counter % 2 === 0) ? 'hello from React': 'Click me')
-        //counter++
+        let count = Number(counter);
+        console.log(count);
+        setButtonText((count % 2 === 0) ? 'hello from React' : initialbuttonText)
+        count++;
+        setCounter(String(count));
+        setClassesList('green-btn')
     }
     return (
-        <div className="app">
-            <button onClick={onButtonClick}>
+        <div className="app" >
+            <button className={classesList} onClick={onButtonClick} counter={counter}>
                 {buttonText}
             </button>
         </div>
@@ -19,4 +26,4 @@ const App = () => {
 
 const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container);
-root.render(<App />)
+root.render(<App initialbuttonText="Click me" counter="0" />)
