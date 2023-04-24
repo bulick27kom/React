bc49534c0f6cd3fd85062f7bde304034074b7956
Roman Brauer <bulick27kom@gmail.com>
2023-04-24 17:26:35 +0300

    10

--- 10-conditional-rendering/src/App.js
+++ 10-conditional-rendering/src/App.js
@@ -4,8 +4,8 @@ import PetInfo from './components/PetInfo';
 function App() {
   return (
     <div className="App">
-      <PetInfo animal="cat" age={7} />
-      <PetInfo animal="fish" age={2} />
+      <PetInfo animal="cat" age={7} hasPet={true} />
+      <PetInfo animal="fish" age={2} hasPet=" " />
     </div>
   )
 }
