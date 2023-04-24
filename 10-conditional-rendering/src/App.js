fda4fbc9970dab5ab17d42c308ce4990e113769a
Roman Brauer <bulick27kom@gmail.com>
2023-04-24 17:39:43 +0300

    10 finish

--- 10-conditional-rendering/src/App.js
+++ 10-conditional-rendering/src/App.js
@@ -4,8 +4,8 @@ import PetInfo from './components/PetInfo';
 function App() {
   return (
     <div className="App">
-      <PetInfo animal="cat" age={3} hasPet={true} />
-      <PetInfo animal="fish" age={1} hasPet />
+      <PetInfo animal="cat" age={3} hasPet />
+      <PetInfo animal="fish" age={1} hasPet={false} />
     </div>
   )
 }
--- 10-conditional-rendering/src/components/PetInfo.js
+++ 10-conditional-rendering/src/components/PetInfo.js
@@ -4,7 +4,7 @@ function PetInfo(props) {
     // const text = hasPet ? `My ${animal} is ${age} years old` : 'I don\'t have an animal'
     return hasPet
         ? <h1> {`My ${animal} is ${age} years old`}</h1>
-        : <h1>I don\'t have an animal</h1>
+        : <h2>I don't have an animal</h2>
 }
 
 export default PetInfo 
