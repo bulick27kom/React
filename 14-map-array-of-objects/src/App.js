e04ae2234c6ad85af5a49f6ec71093f71171506f
Roman Brauer <bulick27kom@gmail.com>
2023-04-25 16:11:08 +0300

    personn component create

--- 14-map-array-of-objects/src/App.js
+++ 14-map-array-of-objects/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 import persons from './data/persons';
+import Person from './components/Person';
 
 function App() {
-  return <div className="App"></div>
+  return <div className="App">
+    {
+      persons.map((person) => {
+        return <Person person={person}/>
+      })
+    }
+  </div>
 }
 
 export default App;
--- 14-map-array-of-objects/src/components/Person.js
+++ 14-map-array-of-objects/src/components/Person.js
@@ -1,7 +1,14 @@
-function Perssn() {
+function Person(props) {
+    const { id, fistName, lastName, email, img } = props.person
     return (
-        <div></div>
+        <div>
+            <p>{id}</p>
+            <p>{fistName}</p>
+            <p>{lastName}</p>
+            <p>{email}</p>
+            <p>{img}</p>
+        </div>
     )
 }
 
-export default Perssn
+export default Person
