7255489fd7cafffcd9f6a7707675eecb7c61f388
Roman Brauer <bulick27kom@gmail.com>
2023-04-25 14:56:41 +0300

    12th progect finished

--- 12-state-via-props/src/App.js
+++ 12-state-via-props/src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import Button from './components/Button';
+import Counter from './components/Counter';
 
 function App() {
-  return <div className="App"></div>
+  return <div className="App">
+    <Counter />
+    <Button />
+    <Button />
+    <Button />
+    <Button />
+  </div>
 }
 
 export default App;
---
+++ 12-state-via-props/src/components/Counter.js
@@ -0,0 +1,7 @@
+function Counter() {
+    return (
+        <h1>Total clicks : 0</h1>
+    )
+}
+
+export default Counter
