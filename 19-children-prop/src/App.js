71f3cfdb8e9df4cc013fb375770f76bded693f2c
Roman Brauer <bulick27kom@gmail.com>
2023-04-26 14:15:31 +0300

    19 done

--- 19-children-prop/src/App.js
+++ 19-children-prop/src/App.js
@@ -3,7 +3,15 @@ import Wrapper from './components/Wrapper/Wrapper';
 
 function App() {
   return <div className="App">
-    <Wrapper />
+    <Wrapper color="lightBlue">
+      <h2>Text inside Wrapper</h2>
+      <button>Click me</button>
+    </Wrapper>
+    <Wrapper color="lightGreen">
+      <h2>Another text inside Wrapper</h2>
+      <p>Some description</p>
+      <input type="text" placeholder="Text here"></input>
+    </Wrapper>
   </div>
 }
 
--- 19-children-prop/src/components/Wrapper/Wrapper.js
+++ 19-children-prop/src/components/Wrapper/Wrapper.js
@@ -1,6 +1,15 @@
-function Wrapper(){
-    return(
-        <div></div>
+function Wrapper(props) {
+    console.log('props :>> ', props);
+    const style = {
+        backgroundColor: props.color,
+        width: '250px',
+        padding: '20px',
+        margin: '20px auto'
+    }
+    return (
+        <div style={style}>
+            {props.children}
+        </div>
     )
 }
 
