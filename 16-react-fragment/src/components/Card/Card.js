9760915eb91a9b6cbf0c1ca6fb96d9ae88792cde
Roman Brauer <bulick27kom@gmail.com>
2023-04-26 08:15:49 +0300

    16-fragments init

---
+++ 16-react-fragment/src/components/Card/Card.js
@@ -0,0 +1,11 @@
+const Card = () => {
+    return (
+        <div>
+            <h1>Hi</h1>
+            <h3>I am a Card</h3>
+            <button>Like!</button>
+        </div>
+    )
+}
+
+export default Card
