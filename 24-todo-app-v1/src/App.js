77de6bded2a18b1470d39a04d244c4d702b7de36
Roman Brauer <bulick27kom@gmail.com>
2023-04-27 19:14:00 +0300

    1

--- 24-todo-app-v1/src/App.js
+++ 24-todo-app-v1/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
+import TodoForm from './components/Todos/TodoForm';
 
 function App() {
-  return <div className="App"></div>
+  return <div className="App">
+    <h1>Todo App</h1>
+    <TodoForm />
+  </div>
 }
 
 export default App;
--- 24-todo-app-v1/src/components/Todos/TodoForm.js
+++ 24-todo-app-v1/src/components/Todos/TodoForm.js
@@ -1,1 +1,12 @@
 
+
+function TodoForm() {
+    return (
+        <div>
+            <input type="text"></input>
+            <button>Submit</button>
+        </div>
+    )
+}
+
+export default TodoForm
